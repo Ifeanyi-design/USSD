6f7b4921f6cae72847f560977170242de389f41d
Ifeanyi-design <ifeanyiagada123@gmail.com>
2025-08-07 02:07:16 +0100

    USSD CODE

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if request.method == "POST":
+        session_id = request.form["sessionId"]
+        service_code = request.form["serviceCode"]
+        phone_number = request.form["phoneNumber"]
+        text = request.form["text"]
+        if text == "":
+            response = "CON What would you want\n"
+            response += "1. My Account No\n"
+            response += "2. My Phone Number"
+        elif text == "1":
+            response = "CON Choose account information you want to view\n"
+            response += "1. Account Number"
+            response += "2. Account Balance"
+
+        elif text == "2":
+            response = f"END your phone number is {phone_number}"
+
+        elif text == "1*1":
+            account_no = "922842242"
+            response = f"END your account number is {account_no}"
+
+        elif text == "1*2":
+            balance = "NG 10000"
+            response = f"END you account balance is {balance}"
+
+        return Response(response, mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
